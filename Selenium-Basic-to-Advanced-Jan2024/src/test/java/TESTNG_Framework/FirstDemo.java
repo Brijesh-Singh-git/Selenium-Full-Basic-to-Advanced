21bf7cda503832e95f5147d84307e582b967308c
Brijesh Singh <abc@amazon.com>
2024-03-25 09:34:59 +0530

    commit

--- Selenium-Basic-to-Advanced-Jan2024/src/test/java/TESTNG_Framework/FirstDemo.java
+++ Selenium-Basic-to-Advanced-Jan2024/src/test/java/TESTNG_Framework/FirstDemo.java
@@ -1,11 +1,41 @@
 package TESTNG_Framework;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class FirstDemo {
+    WebDriver driver;
+    @BeforeClass
+    void openApp(){
+
+
+        System.getProperty("webdriver.edge.driver", "driver.exe");
+        driver = new EdgeDriver();
+
+        driver.manage().timeouts().implicitlyWait(40000, TimeUnit.SECONDS);
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+    }
 
     @Test
-    public void test(){
+    void login(){
+    driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+    driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
 
+
+    }
+
+    @AfterClass
+    void close(){
+
+        driver.quit();
     }
 }
